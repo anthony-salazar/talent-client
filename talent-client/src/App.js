0803c14b79c1acce0049b361ec750b7abda38a78
YaliniNadar <yalini.nadar@gmail.com>
2024-08-30 19:33:59 +0000

    renamed routeConstants.js

--- talent-client/src/App.js
+++ talent-client/src/App.js
@@ -2,16 +2,16 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
-import RouteConstants from './RouteConstants';
+import RouteConstants from './routeConstants';
 import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-         <Route path='/' element={<HomePage/>}/>
-         <Route path='/login' element={<LoginPage/>}/>
-         <Route path='/register' element={<RegisterPage/>}/>
+         <Route path={RouteConstants.Home} element={<HomePage/>}/>
+         <Route path={RouteConstants.Login} element={<LoginPage/>}/>
+         <Route path={RouteConstants.Register} element={<RegisterPage/>}/>
        </Routes>
     </div>
   );
