c3ddea233025fa65eced3f2c8ad6d4a64f6ad5c7
haojun Song <hsong343@gatech.edu>
2024-08-30 16:27:00 +0000

    Add react-router-dom dependency and update npm packages and added app components and home screen

---
+++ talent-client/src/components/Login.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Login() {
+    return (
+        <div className="login-info">
+            <span>User: admin</span>
+            <button className='logout-button'> log out</button>
+        </div>
+
+    )
+}
