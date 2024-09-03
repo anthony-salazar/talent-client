a39be35c5e862e48415978306a183ea20f8436d8
haojun Song <hsong343@gatech.edu>
2024-09-03 18:22:47 +0000

    Remove unused JobSearchBar component and update JobSearchPage

--- talent-client/src/pages/Shared/JobSearchPage.jsx
+++ talent-client/src/pages/Shared/JobSearchPage.jsx
@@ -1,21 +1,43 @@
 import React, {useState} from "react";
-import SearchBar from "../../components/JobSearchBar";
+// import SearchBar from "../../components/JobSearchBar";
 import JobList from "../../components/JobList";
 import list from '../../joblist.json';
 import '../../App.css';
 import Header from "../../components/Header";
 
 export default function JobSearch() {
-    return(
+
+    const jobs = list;
+    const [searchInput, setSearchInput] = useState("");
+
+    const handleSearch = (event) => {
+      setSearchInput(event.target.value);
+    };
+
+    const filteredjobs = jobs.filter((job) => {
+      return job.listing_title
+        .toLowerCase()
+        .includes(searchInput.toLowerCase());
+    });
+
+
+    return (
+      <div>
         <div>
-            <div>
-                <Header />
-            </div>
-            <div className="job-search-page">
-                <h2>Job Search Page</h2>
-                <SearchBar />
-                <JobList jobs={list}/>
-            </div>
+          <Header />
         </div>
+        <div className="job-search-page">
+          <h2>Job Search Page</h2>
+          <div className="search-bar">
+            <input
+              type="text"
+              placeholder="Job Title or Keyword"
+              className="search-input"
+              onChange={handleSearch}
+            ></input>
+          </div>
+          <JobList jobs={filteredjobs} />
+        </div>
+      </div>
     );
 }
