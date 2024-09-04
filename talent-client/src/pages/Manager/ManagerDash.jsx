2f1c5a52f8501161cb9d717d531a816793f5179b
annieneal1 <annieneal05@gmail.com>
2024-09-04 14:48:52 +0000

    made dashboards prettier

--- talent-client/src/pages/Manager/ManagerDash.jsx
+++ talent-client/src/pages/Manager/ManagerDash.jsx
@@ -3,7 +3,8 @@ import {Link} from "react-router-dom";
 import DataTable from "../../components/DataTable";
 import jobs from "../../data/jobs.json";
 import HeaderComponent from '../../components/Header';
-
+import DisplaySettingsIcon from '@mui/icons-material/DisplaySettings';
+import WorkIcon from '@mui/icons-material/Work';
 import {Box, Container, Typography, Button} from '@mui/material';
 
 export default function ManagerDash() {
@@ -12,8 +13,48 @@ export default function ManagerDash() {
             <HeaderComponent />
             <Container sx = {{flex: '1 0 auto', padding:'10px'}}>
                 <Typography variant = 'h4' align = 'center'>Welcome, Hiring Manager</Typography>
-                <DataTable data = {jobs}/>
-                <Button component={Link} to="/manage_jobs">Manage Jobs</Button>
+                <Box
+                    sx={{
+                        display:'flex',
+                        justifyContent: 'center',
+                        gap: 2,
+                        p: 2
+                    }}
+                >
+                    <Button
+                        component={Link}
+                        to="/manage_apps"
+                        variant="contained"
+                        sx={{
+                            display:'flex',
+                            flexDirection:'column',
+                            alignItems:'center',
+                            gap: 1,
+                            p: 2,
+                            textAlign: 'center'
+                        }}
+                    >
+                        <DisplaySettingsIcon sx={{fontSize: 40}}/>
+                        <Typography variant="button">View Applications For Your Postings</Typography>
+                    </Button>
+                    <Button
+                        component={Link}
+                        to="/jobupate"
+                        variant="contained"
+                        sx={{
+                            display:'flex',
+                            flexDirection:'column',
+                            alignItems:'center',
+                            gap: 1,
+                            p: 2,
+                            textAlign: 'center'
+                        }}
+                    >
+                        <WorkIcon sx={{fontSize: 40}}/>
+                        <Typography variant="button">Create A New Job Posting</Typography>
+                    </Button>
+                </Box>
+                
                 
             </Container>
             
