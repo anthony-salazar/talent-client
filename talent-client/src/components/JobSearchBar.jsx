3a37ee0f64bbc686adfe014e7da75d2319e86fec
haojun Song <hsong343@gatech.edu>
2024-09-04 14:26:59 +0000

    Refactor JobSearchBar component to use searchTerm instead of search

--- talent-client/src/components/JobSearchBar.jsx
+++ talent-client/src/components/JobSearchBar.jsx
@@ -4,24 +4,35 @@ import '../App.css';
 import axios from 'axios';
 
 const SearchBar= ( {setJobs} ) => {
-    const [search, setSearch] = useState('');
+    const [searchTerm, setSearch] = useState('');
 
     const handleSearch = () => {
-        axios.get('http://localhost:8080/jobs/search', {params: {search: search}})
-            .then(response => {
-                setJobs(response.data);
-            })
-            .catch(error => {
-                console.error('There was an error!', error);
-            });
+        axios
+          .get("http://localhost:8080/jobs/search", {
+            params: { searchTerm: searchTerm },
+          })
+          .then((response) => {
+            setJobs(response.data);
+          })
+          .catch((error) => {
+            console.error("There was an error!", error);
+          });
     }
 
 
-    return(
-        <div className="search-bar">
-            <input type="text" placeholder="Job Title or Keyword" className="search-input" value = {search} onChange={(e) => setSearch(e.target.value)}/>
-            <button className="search-button" onClick={handleSearch}>Search</button>
-        </div>
+    return (
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Job Title or Keyword"
+          className="search-input"
+          value={searchTerm}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button className="search-button" onClick={handleSearch}>
+          Search
+        </button>
+      </div>
     );
 }
 
