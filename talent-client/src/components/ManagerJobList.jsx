76b55dae68a6c5e77b0448603367d90ef238e5a2
haojun Song <hsong343@gatech.edu>
2024-09-05 01:48:28 +0000

    Completed integration task: 1. working search, delete, and edit 2. working manager  create job 3.  replaced diallogue component using dataform

--- talent-client/src/components/ManagerJobList.jsx
+++ talent-client/src/components/ManagerJobList.jsx
@@ -26,7 +26,7 @@ const ManagerJobList = (props) => {
             </div>
             </div>
             <div className='column'>
-                <JobApplicationDetails job={selectedjob} refresh={props.refresh} setRefresh={props.setRefresh}/>
+                <JobApplicationDetails job={selectedjob}/>
             </div>
 
         </div>
