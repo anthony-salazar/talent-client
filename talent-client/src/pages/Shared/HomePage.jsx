e64e4656764ce3c5ac3073266eee200ecfd27f0d
sakshiseth17 <seth.sakshi1317@gmail.com>
2024-09-05 13:26:33 +0000

    Called all the application API in home page

--- talent-client/src/pages/Shared/HomePage.jsx
+++ talent-client/src/pages/Shared/HomePage.jsx
@@ -9,6 +9,140 @@ import { useEffect } from 'react';
 
 export default function HomePage(props) {
 
+    const applications = async () => {
+        let temp = getAllApplications();
+        console.log(temp)
+    };
+    const applicationByID = async () => {
+        let temp = getApplicationById(100);
+        console.log("applicationByID ",temp)
+    };
+
+    const applicationByMangerID = async () => {
+        let temp = getApplicationByManagerId(1);
+        console.log("applicationByManagerID ",temp)
+    };
+
+    const applicationByJobID = async () => {
+        let temp = getApplicationByJobId(1);
+        console.log("applicationByJobID ",temp)
+    };
+
+    const applicationByUserID = async () => {
+        let temp = getApplicationByUserId(6);
+        console.log("applicationByUserID ",temp)
+    };
+
+    const deleteApplicationByIDHere = async () => {
+        let temp = deleteApplicationById(22);
+        console.log("deleteApplicationByID ",temp)
+    };
+
+    const addApplicationHere = async () => {
+        console.log("In Home add app");
+        //return;
+        let data = {
+            user: {
+                id: 5,
+                username: "jane",
+                password: "jane",
+                type: "Candidate"
+            },
+            job: {
+                id: 1,
+                manager: {
+                    id: 1,
+                    user: {
+                        id: 8,
+                        username: "linda",
+                        password: "linda",
+                        type: "Hiring_Manager"
+                    },
+                    full_name: "Linda Thompson",
+                    email: "linda@example.com",
+                    department: "Human Resources",
+                    phone: "555-1212"
+                },
+                department: "Human Resources",
+                listing_title: "HR Manager Position",
+                date_listed: "2024-08-01T09:00:00",
+                date_closed: "2024-09-15T17:00:00",
+                job_title: "HR Manager",
+                job_description: "Responsible for managing HR processes and overseeing recruitment.",
+                additional_information: "Experience in HR management required. Certification in HR is a plus.",
+                listing_status: "Open"
+            },
+            date_applied: "2024-08-22T12:30:00",
+            cover_letter: "I am very enthusiastic about the HR Manager Position. My experience in digital campaigns and marketing can bring fresh perspectives to HR processes.",
+            custom_resume: "Marketing specialist with a focus on digital campaigns and SEO.",
+            application_status: "Under Review"
+        };
+        let temp = addApplication(data);
+        console.log("addApplication ",temp)
+
+    }
+
+
+    const updateApplicationHere = async () => {
+        console.log("In Home update app");
+        //return;
+        let data = {
+            user: {
+                id: 5,
+                username: "jane",
+                password: "jane",
+                type: "Candidate"
+            },
+            job: {
+                id: 1,
+                manager: {
+                    id: 1,
+                    user: {
+                        id: 8,
+                        username: "linda",
+                        password: "linda",
+                        type: "Hiring_Manager"
+                    },
+                    full_name: "Linda Thompson",
+                    email: "linda@example.com",
+                    department: "Human Resources",
+                    phone: "555-1212"
+                },
+                department: "Human Resources",
+                listing_title: "HR Manager Position",
+                date_listed: "2024-08-01T09:00:00",
+                date_closed: "2024-09-15T17:00:00",
+                job_title: "HR Manager",
+                job_description: "Responsible for managing HR processes and overseeing recruitment.",
+                additional_information: "Experience in HR management required. Certification in HR is a plus.",
+                listing_status: "Open"
+            },
+            date_applied: "2024-08-22T12:30:00",
+            cover_letter: "I am very enthusiastic about the HR Manager Position. My experience in digital campaigns and marketing can bring fresh perspectives to HR processes.",
+            custom_resume: "Marketing specialist with a focus on digital campaigns and SEO.",
+            application_status: "Under Review111111111"
+        };
+        let temp = updateApplication(21,data);
+        console.log("updateApp ",temp)
+
+    }
+
+
+
+
+
+    useEffect(() => {
+        applications();
+        applicationByID();
+        applicationByUserID();
+        applicationByJobID();
+        applicationByMangerID();
+        addApplicationHere();
+        updateApplicationHere();
+        deleteApplicationByIDHere();
+    },[]);
+    //useEffect(() => applications(),[]);
+
     return (
         <Box sx = {{display: 'flex', flexDirection: 'column', height: '100vh'}}>
             <HeaderComponent user={props.user}/>
